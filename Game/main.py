a2220f93d2323b14613dcb3e15c7ef6b7d376582
CrefconGG <miss.sunisa1978@gmail.com>
2023-09-27 15:17:44 +0700

    Edit create screen

---
+++ Game/main.py
@@ -0,0 +1,26 @@
+import pygame as pg
+import constants as c
+
+#initialise pygame
+pg.init()
+
+#create clock
+clock = pg.time.Clock()
+
+#create game window
+screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+pg.display.set_caption("การเอาคืนของป้อม DEMO")
+
+#game loop
+run = True
+while run:
+
+    clock.tick(c.FPS)
+
+    #event control
+    for event in pg.event.get():
+        #exit game
+        if event.type == pg.QUIT:
+            run = False
+
+pg.quit()
--- Main.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.init()
-
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 700
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
